b5b134e6031dced30e8a3773ae64811b3ff8337a
Kaciaryna Dubouskaya <rinawwwster@gmail.com>
2012-08-18 21:30:29 +0300

    Style updates

--- public/js/boot.js
+++ public/js/boot.js
@@ -1,5 +1,10 @@
 $(function() {
-  $(".heading").click(function() {
+  $(".details").click(function() {
     $(this).next(".spoiler").toggle();
-  })
+  });
+  
+   $("a").on( 'click', function() {
+   	var id = $(this).attr("href");
+   	$(id).addClass("clicked");
+   })
 });
