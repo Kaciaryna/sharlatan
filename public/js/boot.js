c4e03d56de1421d7d2430891713f9b83434f2d74
Kaciaryna Dubouskaya <rinawwwster@gmail.com>
2012-09-16 16:26:07 +0300

    Add styles for incut, update js for spoiler

--- public/js/boot.js
+++ public/js/boot.js
@@ -1,11 +1,32 @@
 $(function() {
-  $(".details").click(function() {
-    $(this).next(".spoiler").toggle();
+
+  $(".details").each(function(i, link) {
+    $(link).attr("data-text", "Скрыть подробности");
+    $(link).click(function() {
+      $(this).next(".spoiler").toggle();
+
+      var currentText = $(this).text();
+      var newText = $(this).attr("data-text");
+      $(this).text(newText);
+      $(this).attr("data-text", currentText);
+    });
   });
-  
+
    $("a").on( 'click', function() {
-   	var id = $(this).attr("href");
-   	$(".clicked").removeClass("clicked");
-   	$(id).addClass("clicked");
-   })
+     var id = $(this).attr("href");
+     $(".clicked").removeClass("clicked");
+     $(id).addClass("clicked");
+   });
+
+  $('div.modal').omniWindow({
+    overlay: {
+      hideClass: 'ow_closed'
+    },
+    modal: {
+      hideClass: 'ow_closed'
+    }
+  });
+  $(".incut span").click(function() {
+    $('div.modal').trigger('show');
+  })
 });
